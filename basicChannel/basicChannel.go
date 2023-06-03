df569cb7e2251fd6a34648bc3b39f108c7420caa
Oscar Torres <torresvosc@gmail.com>
2023-06-03 12:16:42 -0600

    Added basic channel example

--- basicChannel/basicChannel.go
+++ basicChannel/basicChannel.go
@@ -1,21 +1,38 @@
 package basicChannel
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Server struct {
 	users  map[string]string
 	userch chan string
+	quitch chan struct{}
 }
 
 func NewServer() *Server {
 	return &Server{
 		users:  make(map[string]string),
 		userch: make(chan string),
+		quitch: make(chan struct{}),
 	}
 }
 
 func (s *Server) Start() {
+	go s.loop()
+}
 
+func (s *Server) loop() {
+free:
+	for {
+		select {
+		case msg := <-s.userch:
+			fmt.Println(msg)
+		case <-s.quitch:
+			fmt.Println("server need to quit")
+			break free
+		}
+	}
 }
 
 func (s *Server) addUser(user string) {
--- basicChannel/basicChannel_test.go
+++ basicChannel/basicChannel_test.go
@@ -1,17 +1,23 @@
 package basicChannel
 
 import (
-	"fmt"
 	"testing"
+	"time"
 )
 
 func TestAddUser(t *testing.T) {
 	server := NewServer()
 	server.Start()
 
-	for i := 0; i < 10; i++ {
+	/* 	for i := 0; i < 10; i++ {
 		go func(i int) {
 			server.userch <- fmt.Sprintf("user_%d", i)
 		}(i)
-	}
+	} */
+	go func() {
+		time.Sleep(2 * time.Second)
+		close(server.quitch)
+	}()
+	// This block
+	select {}
 }
